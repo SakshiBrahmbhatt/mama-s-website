77bacbfad3c1e42e71b38249c058063611c25a20
Sakshi Kalpesh Brahmbhatt <sakshibrahmbhatt999@gmail.com>
2024-10-06 14:36:36 +0530

    Done some changes in home and About pages and Tried some new methods but didn't work

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,6 +2,9 @@ import Content from "../Components/Content";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import HomeSlidder from "../Components/HomeSlider";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouseChimney } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
@@ -26,6 +29,65 @@ export default function Home() {
             className=""
             style={{ padding: "17px 0px 67px" }}
           ></Content>
+          <div className="" style={{ padding: "0px 170px 50px" }}>
+            <table
+              style={{
+                borderCollapse: "collapse",
+                width: "100%",
+                margin: "50px 0",
+              }}
+            >
+              <tr>
+                <td
+                  rowSpan="11"
+                  style={{ paddingRight: "20px", verticalAlign: "top" }}
+                >
+                  <img
+                    src="./sheets.jpg"
+                    style={{ height: "300px", width: "400px" }}
+                  ></img>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h3 style={{ marginBottom: "20px" }}>
+                    ABOUT OUR ORGANIZATION
+                  </h3>
+                </td>
+              </tr>
+              <tr>
+                <td style={{ color: "#888B90" }}>INFINITY INDUSTRIAL FIBER</td>
+              </tr>
+              <tr>
+                <td style={{ color: "#888B90" }}>
+                  Infinity Industrial Fiber is a full-service turnkey solution
+                  provider in the field of corrosion resistant glass fiber
+                  reinforced plastic (FRP) Gratings, Cable Tray, tanks,
+                  canopies, sheets... Founded in{" "}
+                  <Link
+                    to="/About"
+                    className="text-decoration-none"
+                    style={{ color: "#888B90" }}
+                  >
+                    ...Read More
+                  </Link>
+                </td>
+              </tr>
+              <tr>
+                <td style={{padding:"0px 300px"}}>
+                  <button style={{border:"none", borderRadius:"20px", backgroundColor:"black", width:"40px", height:"40px"}}>
+                    <Link
+                      to="/About"
+                      className="text-decoration-none"
+                      style={{ color: "#888B90" }}
+                    >
+                      <FontAwesomeIcon icon={faHouseChimney} />
+                    </Link>
+                  </button>
+                </td>
+              </tr>
+            </table>
+          </div>
         </div>
         <div style={{ backgroundColor: "rgba(0,0,0,0.5)" }}>
           <Content
